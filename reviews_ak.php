db94deef883e8c88d9409b7b313cfa353da62ddc
Anna Kurmanova <annakurmanova0508@gmail.com>
2022-10-19 16:28:56 +0200

    plugin class

--- reviews_ak.php
+++ reviews_ak.php
@@ -9,7 +9,7 @@
  * Description: custom reviews plugin
  * Version: 1.0.0
  * Author: Anna Kurmanova
- * Author URI: https://github.com/annaKurmanova/reviews_ak/
+ * Author URI: https://github.com/annaKurmanova/reviews_ak/tree/master
  * License: GPLv2 or later
  * Text Domain: reviews_ak
  */
@@ -17,3 +17,35 @@
 if(!defined('ABSPATH')) {
   die;
 }
+
+class ReviewsAK {
+  function activate(){
+//generate a custom post type
+  }
+
+  function deactivate() {
+
+  }
+
+  function uninstall() {
+    // delete cpt
+  }
+
+
+}
+
+
+// instance of class
+
+if(class_exists('ReviewsAK')) {
+  $reviewsAk = new ReviewsAK();
+}
+
+
+// activate
+register_activation_hook(__FILE__, array($reviewsAk, 'activate'));
+
+// deactivate
+register_deactivation_hook(__FILE__, array($reviewsAk, 'deactivate'));
+
+// uninstall
