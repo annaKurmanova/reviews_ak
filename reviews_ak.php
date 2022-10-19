dfd32b746defc309b55cc67a030cd8ac3cdf4de8
Anna Kurmanova <annakurmanova0508@gmail.com>
2022-10-19 15:41:01 +0200

    plugin

---
+++ reviews_ak.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *@package reviews_ak
+ */
+
+/**
+ * Plugin Name: reviews_ak
+ * Plusgin URI: https://github.com/annaKurmanova/reviews_ak/tree/master
+ * Description: custom reviews plugin
+ * Version: 1.0.0
+ * Author: Anna Kurmanova
+ * Author URI: https://github.com/annaKurmanova/reviews_ak/
+ * License: GPLv2 or later
+ * Text Domain: reviews_ak
+ */
+
+if(!defined('ABSPATH')) {
+  die;
+}
